554de6c30f0d4699bcf7d166bf03fa760c7f7832
wfercanas <fernando.canas@outlook.com>
2021-07-27 16:44:46 -0500

    add/ Wrap app with Theme Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ThemeProvider } from './Context/ThemeContext';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
-
