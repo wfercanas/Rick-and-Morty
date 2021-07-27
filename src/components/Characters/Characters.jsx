b6a46511f66afeb90146195e551228983a9a152d
wfercanas <fernando.canas@outlook.com>
2021-07-27 11:10:20 -0500

    add/ style for characters without darkmode

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Characters.css';
 
 const Characters = () => {
   const [characters, setCharecters] = useState([]);
@@ -11,7 +12,27 @@ const Characters = () => {
   return (
     <div className="Characters">
       {characters.map((character) => {
-        return <h2>{character.name}</h2>;
+        return (
+          <div className="Character" key={character.id}>
+            <div
+              className="Character__image"
+              style={{ backgroundImage: `url(${character.image})` }}
+              alt="character avatar"
+            />
+            <div className="Character__data">
+              <h2 className="Character__data--name">{character.name}</h2>
+              <p className="Character__data--meta">
+                {character.status} - {character.species}
+              </p>
+              <p className="Character__data--subtitle">Last known location:</p>
+              <p className="Character__data--value">
+                {character.location.name}
+              </p>
+              <p className="Character__data--subtitle">Origin:</p>
+              <p className="Character__data--value">{character.origin.name}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
