20b84c10fa2bb22534d875169959f54389e0130f
wfercanas <fernando.canas@outlook.com>
2021-07-27 10:16:10 -0500

    add/ useEffect class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Characters from './components/Characters';
 
 import Header from './components/Header';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Hola mundo</h1>
+      <Characters />
     </div>
   );
 }
