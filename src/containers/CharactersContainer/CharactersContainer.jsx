b51771e4134019e428fe896613e7e2ea13ce6d1d
wfercanas <fernando.canas@outlook.com>
2021-07-27 21:08:25 -0500

    add/ container component  logic-only

---
+++ src/containers/CharactersContainer/CharactersContainer.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useReducer } from 'react';
+
+import { FavoriteCharacters } from '../../components/FavoriteCharacters/FavoriteCharacters';
+import { Characters } from '../../components/Characters/Characters';
+
+const initialState = {
+  favoriteCharacters: [],
+};
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITES':
+      return {
+        ...state,
+        favoriteCharacters: [...state.favoriteCharacters, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+const CharactersContainer = () => {
+  const [characters, setCharacters] = useState([]);
+  const [state, dispatch] = useReducer(favoriteReducer, initialState);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
+
+  const handleClick = (favoriteCharacter) => {
+    dispatch({ type: 'ADD_TO_FAVORITES', payload: favoriteCharacter });
+  };
+
+  return (
+    <>
+      <FavoriteCharacters favorites={state.favoriteCharacters} />
+      <Characters characters={characters} handleClick={handleClick} />
+    </>
+  );
+};
+
+export { CharactersContainer };
