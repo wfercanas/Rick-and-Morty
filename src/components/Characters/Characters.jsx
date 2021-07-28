dcfc7798574d493ebffab63ad042f7cb58754c92
wfercanas <fernando.canas@outlook.com>
2021-07-27 21:07:27 -0500

    remove/ logic and only render JSX

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,47 +1,52 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../Context/ThemeContext';
 import './Characters.css';
 
-const Characters = () => {
+const Characters = ({ characters, handleClick }) => {
   const [darkmode] = useContext(ThemeContext);
-  const [characters, setCharecters] = useState([]);
-
-  useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character')
-      .then((response) => response.json())
-      .then((data) => setCharecters(data.results));
-  }, []);
 
   return (
-    <div className={darkmode ? 'Characters dark' : 'Characters'}>
-      {characters.map((character) => {
-        return (
-          <div
-            className={darkmode ? 'Character dark' : 'Character'}
-            key={character.id}
-          >
-            <div
-              className="Character__image"
-              style={{ backgroundImage: `url(${character.image})` }}
-              alt="character avatar"
-            />
-            <div className="Character__data">
-              <h2 className="Character__data--name">{character.name}</h2>
-              <p className="Character__data--meta">
-                {character.status} - {character.species}
-              </p>
-              <p className="Character__data--subtitle">Last known location:</p>
-              <p className="Character__data--value">
-                {character.location.name}
-              </p>
-              <p className="Character__data--subtitle">Origin:</p>
-              <p className="Character__data--value">{character.origin.name}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h2>List of characters</h2>
+      <section className={darkmode ? 'Characters dark' : 'Characters'}>
+        {characters.map((character) => {
+          return (
+            <article
+              className={darkmode ? 'Character dark' : 'Character'}
+              key={character.id}
+            >
+              <div
+                className="Character__image"
+                style={{ backgroundImage: `url(${character.image})` }}
+              />
+              <div className="Character__data">
+                <h2 className="Character__data--name">{character.name}</h2>
+                <p className="Character__data--meta">
+                  {character.status} - {character.species}
+                </p>
+                <p className="Character__data--subtitle">
+                  Last known location:
+                </p>
+                <p className="Character__data--value">
+                  {character.location.name}
+                </p>
+                <p className="Character__data--subtitle">Origin:</p>
+                <p className="Character__data--value">
+                  {character.origin.name}
+                </p>
+                <i
+                  className="Character__favButton"
+                  onClick={() => handleClick(character)}
+                >
+                  🤍
+                </i>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </>
   );
 };
 
-export default Characters;
+export { Characters };
