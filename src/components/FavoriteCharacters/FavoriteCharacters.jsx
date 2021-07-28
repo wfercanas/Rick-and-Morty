d52d3c6a911e1ffabf6c1dfa0ad669947ef2212a
wfercanas <fernando.canas@outlook.com>
2021-07-27 21:42:42 -0500

    add/ favorite character avatars style

--- src/components/FavoriteCharacters/FavoriteCharacters.jsx
+++ src/components/FavoriteCharacters/FavoriteCharacters.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../Context/ThemeContext';
+
+import './FavoriteCharacters.css';
 
 const FavoriteCharacters = ({ favorites }) => {
+  const [darkmode] = useContext(ThemeContext);
   return (
     <>
-      <h2>Favorites</h2>
-      <ul>
+      <h2 className={darkmode ? 'Title dark' : 'Title'}>Favorites</h2>
+      <ul className={darkmode ? 'Favorites dark' : 'Favorites'}>
         {favorites.map((favorite) => (
-          <li>{favorite.name}</li>
+          <li key={favorite.id} className="Favorites__avatar">
+            <img
+              src={favorite.image}
+              className="Favorites__avatar--image"
+              alt="Avatar of character"
+            />
+          </li>
         ))}
       </ul>
     </>
